1ddfa599df97247c4336e34ed2d2aede67cf9d36
m-weinzettl <054057@edu.szf.at>
2026-02-11 11:28:20 +0100

    git befehle added

---
+++ git befehle.py
@@ -0,0 +1,14 @@
+
+
+
+
+
+
+# git init
+# git add .
+# git commit -m ""
+# git remote add origin <url>
+# git push -u origin main
+
+# git branch -M main  (master branch in main umbenennen)
+# git push -u origin main --force (remote main branch mit lokalem main branch überschreiben)
